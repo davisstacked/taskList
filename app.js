67ae0db36dea0581bac7ff51bc469f7af2116f0c
davisstacked <brettzvi@gmail.com>
2021-03-29 08:34:36 -0700

    remove task

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
 }
 
 // Add Task
@@ -39,8 +41,17 @@ function addTask(e) {
   taskList.appendChild(li);
 
   // //Clear input
-  // taskListInput.value = "";
+  taskInput.value = "";
 
   // default is a form submit 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();    
+    }
+  }
 }
